4ec306d565e31a48113c46b0e091f0aa186f9779
srojgam0907 <srojgam0907@g.educaand.es>
2026-01-15 10:52:51 +0100

    Clase Telefono

--- src/uml_practica2/Telefono.java
+++ src/uml_practica2/Telefono.java
@@ -1,5 +1,16 @@
 package uml_practica2;
 
 public class Telefono {
+	private String numero;
+	private TipoTelefono tipo;
 
+	public Telefono(String numero, TipoTelefono tipo) {
+		this.numero = numero;
+		this.tipo = tipo;
+	}
+
+	@Override
+	public String toString() {
+		return this.tipo + ": " + this.numero;
+	}
 }
