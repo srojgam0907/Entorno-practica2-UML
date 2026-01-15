33463ed236feeea93543099ee277c236fa3d52fb
srojgam0907 <srojgam0907@g.educaand.es>
2026-01-15 10:52:29 +0100

    Clase Contacto

--- src/uml_practica2/Contacto.java
+++ src/uml_practica2/Contacto.java
@@ -1,5 +1,71 @@
 package uml_practica2;
 
-public class Contacto {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Contacto {
+	private int id;
+	private String nombre;
+	private String apellidos;
+	private String email;
+	private Direccion direccion;
+	private List<Telefono> telefonos;
+
+	public Contacto(int id, String nombre, String apellidos, String email, Direccion direccion) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.email = email;
+		this.direccion = direccion;
+		this.telefonos = new ArrayList<>();
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public String getApellidos() {
+		return this.apellidos;
+	}
+
+	public void agregarTelefono(Telefono t) {
+		this.telefonos.add(t);
+	}
+
+	public List<Telefono> getTelefonos() {
+		return new ArrayList<>(this.telefonos);
+	}
+
+	public Direccion getDireccion() {
+		return this.direccion;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("ID: ").append(this.id).append("\n");
+		sb.append("Nombre: ").append(this.nombre).append(" ").append(this.apellidos).append("\n");
+
+		if (!this.email.isBlank()) {
+			sb.append("Email: ").append(this.email).append("\n");
+		}
+
+		sb.append("Dirección: ").append(this.direccion).append("\n");
+
+		sb.append("Teléfonos:\n");
+		if (this.telefonos.isEmpty()) {
+			sb.append("  (sin teléfonos)\n");
+		} else {
+			for (Telefono t : this.telefonos) {
+				sb.append("  - ").append(t).append("\n");
+			}
+		}
+
+		return sb.toString();
+	}
 }
